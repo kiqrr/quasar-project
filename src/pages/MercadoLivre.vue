76d7e74502853512b024683f0df36084f36958e3
axtecabr <caiquerr99@gmail.com>
2024-08-01 14:04:34 -0300

    added modal for CEP btn and gradient/background-color

--- src/pages/MercadoLivre.vue
+++ src/pages/MercadoLivre.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="">
+  <q-page class="" style="background-color: #ebebeb">
     <header>
       <div class="full-width" style="height: 100px; background-color: #ffe600">
         <div class="row flex-center">
@@ -9,15 +9,50 @@
               spinner-color="white"
               style="height: 34px; width: 134px"
             />
-            <div class="">Informe seu CEP</div>
+            <q-btn
+              label="Informe seu CEP"
+              no-caps
+              flat
+              style="color: #000"
+              @click="prompt = true"
+            />
+            <q-dialog v-model="prompt" persistent>
+              <q-card style="min-width: 650px">
+                <q-space />
+                <q-btn icon="close" flat round dense v-close-popup />
+                <q-card-section>
+                  <div class="text-h6">
+                    Selecione onde quer receber suas compras
+                  </div>
+                  <div class="text-h7">
+                    Você poderá ver custos e prazos de entrega precisos em tudo
+                    que procurar.
+                  </div>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                  <q-input
+                    dense
+                    v-model="address"
+                    autofocus
+                    @keyup.enter="prompt = false"
+                  />
+                </q-card-section>
+
+                <q-card-actions align="right" class="text-primary">
+                  <q-btn flat label="Cancel" v-close-popup />
+                  <q-btn flat label="Add address" v-close-popup />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
           </div>
           <div class="q-mx-xl">
             <q-input
-              filled
+              outlined
               dense
               bg-color="white"
               placeholder="Buscar produtos, marcas e muito mais..."
-              color=""
+              color="grey"
               v-model="text"
               :readonly="readonly"
               :disable="disable"
@@ -37,7 +72,10 @@
               <q-btn no-caps flat style="color: #000" label="Contato" />
             </div>
           </div>
-          <div class="q-mt-md">
+          <div
+            class="q-mt-md"
+            style="font-family: Arial, Helvetica, sans-serif"
+          >
             <q-img
               src="src/assets/D_NQ_904735-MLA77640197882_072024-OO.webp"
               spinner-color="white"
@@ -46,10 +84,11 @@
             <q-btn no-caps flat style="color: #000" label="Crie a sua conta" />
             <q-btn no-caps flat style="color: #000" label="Entre" />
             <q-btn no-caps flat style="color: #000" label="Compras" />
+            <q-btn flat round color="black" icon="local_grocery_store" />
           </div>
         </div>
       </div>
-      <div class="">
+      <div class="" style="">
         <q-carousel
           animated
           v-model="slide"
@@ -61,6 +100,7 @@
           transition-next="slide-left"
           @mouseenter="autoplay = false"
           @mouseleave="autoplay = true"
+          class="carousel-gradient"
         >
           <q-carousel-slide
             :name="1"
@@ -80,6 +120,20 @@
   </q-page>
 </template>
 
+<style scoped>
+.carousel-gradient::after {
+  content: "";
+  position: absolute;
+  top: 70%;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(180deg, transparent, #ebebeb);
+  pointer-events: none;
+  z-index: 1;
+}
+</style>
+
 <script>
 import { ref } from "vue";
 
@@ -88,6 +142,11 @@ export default {
     return {
       slide: ref(1),
       autoplay: ref(true),
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+
+      address: ref(""),
     };
   },
 };
